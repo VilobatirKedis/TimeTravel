d58350552fdf95de8bfa46f3fbd39b4a662893a7
VilobatirKedis <vilobatirk@gmail.com>
2022-06-01 18:32:47 +0200

    Minor changes

--- lib/screens/explore/main.dart
+++ lib/screens/explore/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sizer/sizer.dart';
 import 'package:time_travel/utils/constants.dart';
 import 'package:time_travel/utils/markers.dart';
 
@@ -12,7 +14,77 @@ class ExplorePage extends StatelessWidget {
       color: kMainColor,
       child: Column(
         children: [
-          data.image
+          ClipRRect(
+            borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(10), bottomRight: Radius.circular(10)),
+            child: data.image,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 2.h),
+            child: Text(
+              data.title,
+              style: GoogleFonts.montserrat(
+                textStyle: Theme.of(context).textTheme.headline4,
+                fontSize: 25.sp,
+                fontWeight: FontWeight.w200,
+                color: Colors.white
+              ),
+            ),
+          ),
+          Card(
+            color: kMainColor.withOpacity(0.6),
+            child: Row(
+              children: [
+                Text("Teatro"), 
+              ]
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Text(
+                "TeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatro",
+                style: GoogleFonts.montserrat(
+                  color: Colors.white,
+                  fontSize: 15.sp
+                ),
+              )
+            ),
+          )
+        ],
+      )
+    );
+
+
+    return Container(
+      color: kMainColor,
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(10), bottomRight: Radius.circular(10)),
+            child: data.image,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 2.h),
+            child: Text(
+              data.title,
+              style: GoogleFonts.montserrat(
+                textStyle: Theme.of(context).textTheme.headline4,
+                fontSize: 25.sp,
+                fontWeight: FontWeight.w200,
+                color: Colors.white
+              ),
+            ),
+          ),
+          Card(
+            color: kMainColor.withOpacity(0.6),
+            child: Row(
+              children: [
+                Text("Teatro"), 
+              ]
+            ),
+          ),
+          const SingleChildScrollView(
+            child: Text("TeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatroTeatro")
+          ),
         ],
       ),
     );
--- lib/screens/settings/main.dart
+++ lib/screens/settings/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sizer/sizer.dart';
+import 'package:time_travel/utils/constants.dart';
 
 
 class Settings extends StatelessWidget {
@@ -7,7 +10,26 @@ class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      color: kMainColor,
+      child: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 5.h, left: 5.w),
+              child: Text(
+                "Settings.",
+                style: GoogleFonts.montserrat(
+                  textStyle: Theme.of(context).textTheme.headline4,
+                  fontSize: 30.sp,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
